90f1aba05e2ef8661b75bdae774350a00aa73f41
Mr-fuaaaadh <fuhadmk4@gmail.com>
2026-01-05 14:38:39 +0530

    dtabase

--- config/settings.py
+++ config/settings.py
@@ -88,12 +88,24 @@ AUTH_USER_MODEL = "staff.Staff"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cict_db',
+        'USER': 'cict_user',
+        'PASSWORD': 'cict@123!!@@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
